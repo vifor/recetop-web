e3cb93d044a0ecd5ff09e54dd99200ee5731ae51
Victoria Forte <victoria.pocladova@gmail.com>
2025-09-13 10:25:39 -0300

    chore: Remove unnecessary comment

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
    output: 'export',
-  // Es posible que necesites añadir la siguiente línea por el componente <Image>
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+export default nextConfig
