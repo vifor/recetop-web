c23f513e439b9c9b7acc93d4cfa76c7605f8c170
Victoria Forte <victoria.pocladova@gmail.com>
2025-09-13 09:39:04 -0300

    feat: Add LICENSE and update README

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   output: 'export',
+  // Es posible que necesites añadir la siguiente línea por el componente <Image>
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
